6f664c5b8a0742de07f6d69d3f4fd4af981d849a
Alexandre Medeiros <alexandre@foccus.cc>
2017-08-04 19:03:16 -0300

    - Finalizar JS

---
+++ _src/scripts/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+setTimeout(function () {
+    console.log("ES6 FTW - teste");
+}, 1000);
+
+// import {jQuery as $} from 'jquery';
+
+// import bootsrap from 'bootstrap-sass';
+//
+// $(function() {
+//
+//     console.log('Teste - Teste');
+//
+// });
